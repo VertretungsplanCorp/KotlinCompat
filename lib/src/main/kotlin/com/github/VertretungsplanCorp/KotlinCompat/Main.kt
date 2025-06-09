6c5d26a162cf0a71053837688c36398dcf06c115
= <seppi@neureiter.bayern>
2025-06-09 12:02:29 +0200

    fixed api

--- lib/src/main/kotlin/com/github/VertretungsplanCorp/KotlinCompat/Main.kt
+++ lib/src/main/kotlin/com/github/VertretungsplanCorp/KotlinCompat/Main.kt
@@ -1,12 +1,14 @@
 package com.github.VertretungsplanCorp.KotlinCompat.vpkotlincompat
 
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.plugins.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
 
+// This is the Server class that handles all the requests.
+// The parameter url sets the url of the api.
 class Server(url: String) {
   val client: HttpClient = HttpClient(OkHttp) { defaultRequest { url(url) } }
-  suspend fun getPing() = client.get("/getping").toString()
+
+  suspend fun getPing() = client.get("/ping").toString()
 }
